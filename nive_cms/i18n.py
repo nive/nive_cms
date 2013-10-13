7a32295a241519a840612085439cd767a576cad5
adroullier <arndt@nive.co>
2013-10-13 14:48:50 +0200

    added i18n

---
+++ nive_cms/i18n.py
@@ -0,0 +1,9 @@
+"""
+use as import:
+from nive_cms.i18n import _
+"""
+from translationstring import TranslationStringFactory
+_ = TranslationStringFactory('nive_cms')
+
+from nive.i18n import translator
+from nive.i18n import translate
