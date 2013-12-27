8a184537039f74fd215d7dd5c82e827972c163a1
adroullier <arndt@nive.co>
2013-12-27 16:29:57 +0100

    test fix

--- nive_cms/cmsview/tests/test_configuration.py
+++ nive_cms/cmsview/tests/test_configuration.py
@@ -33,10 +33,3 @@ class TestConf(unittest.TestCase):
         print FormatConfTestFailure(r)
         self.assert_(False, "Configuration Error")
 
-    def test_conf3(self):
-        r=view.editorAssetsConfiguration.test()
-        if not r:
-            return
-        print FormatConfTestFailure(r)
-        self.assert_(False, "Configuration Error")
-
